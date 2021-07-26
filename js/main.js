327c5c834656d320edbdad3e8758e86d1ced4e8a
SonNA92 <luuson.neu@gmail.com>
2021-07-26 11:04:05 +0700

    Ford layout

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('btncollapse').onclick = function(){
+    document.getElementById('disclosures-content').style.height = '300px';
+    // document.getElementById('disclosures-content').style.height = '100%';
+
+}
